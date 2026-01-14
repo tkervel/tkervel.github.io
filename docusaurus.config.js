c579c42905d4cfb51f981e3cff91cfc9b621a6ac
Tristelle Kervel <tkervel@gmail.com>
2026-01-14 11:27:15 +0100

    Testing v1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://tkervel.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tkervel.github.io',
+  baseUrl: '/tkervel.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="portfolio/intro">
             Explore my portfolio 🔍
           </Link>
         </div>
